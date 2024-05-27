536a77ea5644a2ee5b111b6e1b7e0e76cc11f618
Yeming Hu <yeminghuhym@gmail.com>
2024-05-26 21:55:52 -0400

    Metadata

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,5 @@
 import { type ClassValue, clsx } from "clsx";
+import { Metadata } from "next";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -13,3 +14,33 @@ export const formatPrice = (price: number) => {
 
   return formatter.format(price);
 };
+
+export function constructMetadata({
+  title = "CaseCrafter - custom high-qulity phone cases",
+  description = "Create custom high-qulity phone cases with CaseCrafter. Choose from a variety of materials and finishes to make your phone case truly unique.",
+  image = "/thumbnail.png",
+  icons = "/favicon.ico",
+}: {
+  title?: string;
+  description?: string;
+  image?: string;
+  icons?: string;
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [{ url: image }],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [image],
+      creator: "jonney",
+    },
+    icons,
+  };
+}
