c1e4ce562d1fb75de7cdd2b774885d740029f6bb
Yeming Hu <yeminghuhym@gmail.com>
2024-05-28 20:22:47 -0400

    updated webhooks

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -20,9 +20,13 @@ sgMail.setApiKey(sendGridApiKey);
 export async function POST(req: Request) {
   try {
     const body = await req.text();
+    console.log("Request Body:", body); // Log the request body
+
     const signature = headers().get("stripe-signature");
+    console.log("Stripe Signature:", signature); // Log the stripe signature
 
     if (!signature) {
+      console.log("Invalid signature"); // Log if the signature is missing
       return new Response("Invalid signature", { status: 400 });
     }
 
@@ -32,8 +36,11 @@ export async function POST(req: Request) {
       process.env.STRIPE_WEBHOOK_SECRET!
     );
 
+    console.log("Webhook Event:", event); // Log the webhook event
+
     if (event.type === "checkout.session.completed") {
       if (!event.data.object.customer_details?.email) {
+        console.log("Missing user email"); // Log if user email is missing
         throw new Error("Missing user email");
       }
 
@@ -121,12 +128,19 @@ export async function POST(req: Request) {
         html: emailContent,
       };
 
-      await sgMail.send(msg);
+      console.log("Sending email with SendGrid:", msg); // Log the email message before sending
+
+      try {
+        await sgMail.send(msg);
+        console.log("Email sent successfully"); // Log if email sent successfully
+      } catch (sendError) {
+        console.error("Error sending email:", sendError); // Log any errors during sending email
+      }
     }
 
     return NextResponse.json({ result: event, ok: true });
   } catch (error) {
-    console.error(error);
+    console.error("Error:", error);
     return NextResponse.json(
       { message: "Something went wrong", ok: false },
       { status: 500 }
